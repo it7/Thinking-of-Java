e1cde3d2d70cab2e0ef8df12bb0c4f8cc8929b20
Kristina K <kristina379@mail.ru>
2015-07-25 17:22:43 +0300

    переменный список аргументов

---
+++ 9. переменный список аргументов.java
@@ -0,0 +1,25 @@
+/**
+ * список аргументов переменной длины"
+ *  System.out.println("");
+ */
+
+import java.lang.*;
+
+class A { int i;}
+
+public class flo {
+    static void printArray(Object... args) {
+            for(Object obj : args)
+                System.out.println(obj + " ");
+            System.out.println();
+    }
+    public static void main(String[] args){
+        printArray( new Integer(47), new Float(3.14),
+                new Double(11.11));
+        printArray("one", "two", "three");
+        printArray(new A(), new A());
+        printArray((Object[])new Integer[]{1, 2, 3, 4});
+        printArray();
+    }
+}
+
